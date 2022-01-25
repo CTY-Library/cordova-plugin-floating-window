0625233f5aac86734baaa72e506a32571c5bf8ca
陈楼乃 <chenlounai@zzltop.com>
2022-01-25 19:01:50 +0800

    相对布局,双击事件

--- src/android/FloatingVideoService.java
+++ src/android/FloatingVideoService.java
@@ -60,6 +60,20 @@ public class FloatingVideoService extends Service  {
   public static RelativeLayout video_display_relativeLayout;
   public static int  landscape = 1;
 
+  public static int  iCountViewBigger = 2;
+  public static int  iCountViewShow = 1;
+  private static final int TOUCH_MOVE = 1;//自定义移动
+  private static final int TOUCH_CLICK = 2;//自定义单击
+  private static final int TOUCH_DOUBLE_CLICK = 3;//自定义双击
+  private static final int  TOUCH_ACTION_DOWN = 4;
+
+  private static final long singleClickDurationTime = 200;//00;//常量200ms，抬按时间差
+  private static final long doubleClickDurationTime = 300;//00;//常量300ms，两次点击时间差
+
+  private long touchDownTime = -1;//触点按下时间
+  //private long touchUpTIme = -1;//触点抬起时间
+  private long lastSingleClickTime = -1;//上次发生点击的时刻
+
   @Override
   public void onCreate() {
     super.onCreate();
@@ -74,16 +88,16 @@ public class FloatingVideoService extends Service  {
     layoutParams.format = PixelFormat.RGBA_8888;
     layoutParams.gravity = Gravity.LEFT | Gravity.TOP;
     layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
-    
+
     layoutParams.x = 100;
     layoutParams.y = 100;
 
     if(landscape==1){
-      layoutParams.width = 800;
-      layoutParams.height = 450;
+      layoutParams.width = 300*iCountViewBigger;
+      layoutParams.height = 150*iCountViewBigger;
     }else{
-      layoutParams.width = 450;
-      layoutParams.height = 800;
+      layoutParams.width = 150*iCountViewBigger;
+      layoutParams.height = 300*iCountViewBigger;
     }
 
     mediaPlayer = new MediaPlayer();
@@ -101,6 +115,68 @@ public class FloatingVideoService extends Service  {
     showFloatingWindow();
     return super.onStartCommand(intent, flags, startId);
   }
+
+
+
+  //合成自定义的事件类型
+  private int customTouchType(MotionEvent event){
+    //要返回的自定义事件类型值
+    int touchType = -1;
+    //得到原始触屏事件动作
+    int action = event.getAction();
+    //若发生触屏移动原始动作
+    if(action == MotionEvent.ACTION_MOVE){
+//      //得到当前时刻与上一次按下时刻时间差
+//      long deltaTime = System.currentTimeMillis() - touchDownTime;
+//      //只有当抬起和按下时间差大于200ms才被认定为自定义触屏事件
+//      if(deltaTime > singleClickDurationTime){
+//        //自定义触屏移动事件
+//        touchType = TOUCH_MOVE;
+//      }
+      touchType = TOUCH_MOVE;
+    }
+    //若发生按下原始事件
+    else if(action == MotionEvent.ACTION_DOWN){
+      //记录下按下时间
+      touchDownTime = System.currentTimeMillis();
+      touchType = TOUCH_ACTION_DOWN;
+    }
+    //若发生抬起原始事件
+    else if(action == MotionEvent.ACTION_UP){
+      //记录下抬起时间
+      long touchUpTime = System.currentTimeMillis();
+      //抬起按下的时间差
+      long downUpDurationTime = touchUpTime - touchDownTime;
+      //downUpDurationTime = downUpDurationTime / 100000 / 10000;
+      //若抬起按下时间差小于200ms则表示发生点击事件
+      if(downUpDurationTime <= singleClickDurationTime){
+        //计算这次抬起事件距离上次点击的时间差
+        long twoClickDurationTime = touchUpTime - lastSingleClickTime;
+        //twoClickDurationTime = twoClickDurationTime / 100000000;
+        //若两次点击事件时间差小于300ms则表示发生双击事件
+        if(twoClickDurationTime <=  doubleClickDurationTime){
+          //自定义双击事件
+          touchType = TOUCH_DOUBLE_CLICK;
+          //由于已经确定最近的上一次点击事件的类型，可以重置变量
+          lastSingleClickTime = -1;
+          touchDownTime = -1;
+          touchUpTime = -1;
+        }
+        //若两次点击事件时间差大于300ms，则之前那次的点击一定是单击，
+        //而这次触屏点击需要经过300ms后才能判断类型到底是不是双击
+        else{
+          //保存本次点击事件时刻
+          lastSingleClickTime = touchUpTime;
+          touchType = TOUCH_CLICK;
+        }
+      }else {
+        touchType = TOUCH_CLICK;
+      }
+    }
+    //返回自定义事件类型
+    return touchType;
+  }
+
 
   @RequiresApi(api = Build.VERSION_CODES.M)
   public static void hideVideo()
@@ -224,6 +300,21 @@ public class FloatingVideoService extends Service  {
         }
       });
 
+//      surfaceView.setOnClickListener( new  ImageView.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//          iCountViewShow++;
+//          // 显示,隐藏 按钮
+//          if(iCountViewShow % 2 == 0) {
+//            closeImageView.setVisibility(View.VISIBLE);
+//            goMainImageView.setVisibility(View.VISIBLE);
+//          }else{
+//            closeImageView.setVisibility(View.GONE);
+//            goMainImageView.setVisibility(View.GONE);
+//          }
+//        }
+//      });
+
       windowManager.addView(displayView, layoutParams);
     }
   }
@@ -234,32 +325,65 @@ public class FloatingVideoService extends Service  {
     private int x;
     private int y;
 
+
     @Override
     public boolean onTouch(View view, MotionEvent event) {
-      switch (event.getAction()) {
-        case MotionEvent.ACTION_CANCEL:
-          //isStarted = false; //todo
-          break;
-        case MotionEvent.ACTION_DOWN:
-          x = (int) event.getRawX();
-          y = (int) event.getRawY();
-          break;
-        case MotionEvent.ACTION_MOVE:
-          int nowX = (int) event.getRawX();
-          int nowY = (int) event.getRawY();
-          int movedX = nowX - x;
-          int movedY = nowY - y;
-          x = nowX;
-          y = nowY;
-          layoutParams.x = layoutParams.x + movedX;
-          layoutParams.y = layoutParams.y + movedY;
-          windowManager.updateViewLayout(view, layoutParams);
-          break;
+      ImageView  goMainImageView =   displayView.findViewById(R.id.iv_zoom_main_btn);
+      ImageView closeImageView =   displayView.findViewById(R.id.iv_close_window);
 
-        default:
-          break;
+      //通过调用customTouchType方法，得到我们自定义的事件类型
+      int touchType = customTouchType(event);
+      //若自定义触屏事件是触屏移动
+      if(touchType == TOUCH_ACTION_DOWN) {
+        //当发生触屏移动事件，执行某方法
+        x = (int) event.getRawX();
+        y = (int) event.getRawY();
       }
+      else if(touchType == TOUCH_MOVE) {
+        //当发生触屏移动事件，执行某方法
+        int nowX = (int) event.getRawX();
+        int nowY = (int) event.getRawY();
+        int movedX = nowX - x;
+        int movedY = nowY - y;
+        x = nowX;
+        y = nowY;
+        layoutParams.x = layoutParams.x + movedX;
+        layoutParams.y = layoutParams.y + movedY;
+        windowManager.updateViewLayout(view, layoutParams);
+
+      }
+      //若自定义触屏事件为双击事件
+      else if(touchType == TOUCH_DOUBLE_CLICK){
+        //当发生双击事件，执行某方法
+        if(iCountViewBigger>=1 && iCountViewBigger <= 2){
+          iCountViewBigger++;
+        }else{
+          iCountViewBigger--;
+        }
+        if(landscape==1){
+          layoutParams.width = 300*iCountViewBigger;
+          layoutParams.height = 150*iCountViewBigger;
+        }else{
+          layoutParams.width = 150*iCountViewBigger;
+          layoutParams.height = 300*iCountViewBigger;
+        }
+      }
+      else if( touchType == TOUCH_CLICK){
+        iCountViewShow++;
+        // 显示,隐藏 按钮
+        if(iCountViewShow % 2 == 0) {
+          closeImageView.setVisibility(View.VISIBLE);
+          goMainImageView.setVisibility(View.VISIBLE);
+        }else{
+          closeImageView.setVisibility(View.GONE);
+          goMainImageView.setVisibility(View.GONE);
+        }
+      }
+ 
       return true;
+
+
+
     }
   }
 }
